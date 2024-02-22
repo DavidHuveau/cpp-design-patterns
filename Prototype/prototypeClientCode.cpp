bcc448699bf59c95235e93dcdbe40abe5dc6e913
david huveau <davidhuveau@mac171even32.eventmaker>
2024-02-22 09:10:38 +0100

    copy shapes

--- Prototype/prototypeClientCode.cpp
+++ Prototype/prototypeClientCode.cpp
@@ -1,17 +1,26 @@
 #include "prototypeClientCode.hpp"
-
 #include "Rectangle.hpp"
 #include "Circle.hpp"
 #include "Shape.hpp"
+
+using namespace std; 
 
 void prototypeClientCode() {
   Rectangle prototypeRectangle(10, 10, "red", 200, 50);
   Shape* clonedRectangle1 = prototypeRectangle.clone();
   Shape* clonedRectangle2 = prototypeRectangle.clone();
-  clonedRectangle1->draw();
-  clonedRectangle2->draw();
 
   Circle prototypeCircle(10, 10, "blue", 120);
   Shape* clonedCircle1 = prototypeCircle.clone();
-  clonedCircle1->draw();
+
+  vector<Shape*> shapes = {clonedRectangle1, clonedRectangle2, clonedCircle1};
+
+  vector<Shape*> shapesCopy;
+  for (short int i(0); i < shapes.size(); i++) {
+    shapesCopy.push_back(shapes[i]->clone());
+  }
+
+  for (int i = 0; i < shapesCopy.size(); ++i) {
+    shapesCopy[i]->draw();
+  }
 };
